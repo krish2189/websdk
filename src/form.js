88abe48d58881d743e5e01b7f742c0a52e169deb
krish2189 <92054498+krish2189@users.noreply.github.com>
2024-10-06 20:17:39 +0530

    added Registration Fields bundled JS project

--- src/form.js
+++ src/form.js
@@ -2,34 +2,13 @@
 import { createUser } from './utils/api.js';
 
 export function renderRegistrationForm(targetElement) {
-    const formHtml = `
-    <form id="registrationForm" class="form-container">
-      <h2>Registration Form</h2>
-      
-      <label for="name">Name:</label>
-      <input type="text" id="name" placeholder="Enter your name" required/>
-  
-      <label for="email">Email:</label>
-      <input type="email" id="email" placeholder="Enter your email" required/>
-  
-      <label for="dob">Date of Birth:</label>
-      <input type="date" id="dob" required/>
-  
-      <label for="postalCode">Postal Code:</label>
-      <input type="text" id="postalCode" placeholder="Enter your postal code" required/>
-  
-      <label for="gender">Gender:</label>
-      <select id="gender" required>
-        <option value="" disabled selected>Select your gender</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="other">Other</option>
-      </select>
-  
+  const formHtml = `
+    <form id="registrationForm">
+      <input type="text" id="name" placeholder="Name" required/>
+      <input type="email" id="email" placeholder="Email" required/>
       <button type="submit">Submit</button>
     </form>
   `;
-  
 
   targetElement.innerHTML = formHtml;
 
